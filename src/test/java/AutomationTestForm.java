26df0d08d4af0c63cd0ae06fb4c6811c9ddf4483
Undgrnd23 <undgrnd23@gmail.com>
2024-03-09 21:06:54 +0300

    Add new tests

--- src/test/java/AutomationTestForm.java
+++ src/test/java/AutomationTestForm.java
@@ -1,4 +1,32 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Configuration.*;
+import static com.codeborne.selenide.Selenide.*;
+
 public class AutomationTestForm {
 
+    @BeforeAll
+    static void beforeAll() {
+        baseUrl = "https://demoqa.com";
+        holdBrowserOpen = true;
+    }
 
+    @Test
+    void fillFormTest() {
+        open("/automation-practice-form");
+        $("#firstName").setValue("Neo");
+        $("#lastName").setValue("Matricev");
+        $("#userEmail").setValue("neo-matricev@matrix.com");
+        $(".custom-control-label").shouldHave(text("Male")).click();
+        $("#userNumber").setValue("+777777777");
+
+
+
+
+
+
+
+    }
 }
